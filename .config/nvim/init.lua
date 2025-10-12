b43509a7ce69184cdb5cbb15e4ef46dd1fd1b608
Himanshu <manshu3220@gmail.com>
2025-10-12 18:42:41 +0530

    Updated init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,148 +1,143 @@
 vim.g.mapleader = " "
 -- vim.pack.update()
 vim.pack.add({
-  "https://github.com/MeanderingProgrammer/render-markdown.nvim.git",
-  "https://github.com/folke/flash.nvim",
   "https://github.com/chomosuke/typst-preview.nvim",
-  "https://github.com/folke/which-key.nvim",
-  "https://github.com/voldikss/vim-floaterm",
   "https://github.com/h-jangra/bare.min",
 })
 require("bare")
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function() require("render-markdown").setup() end,
-})
+-------------------------------------------------
+-- Options
+-------------------------------------------------
+local opt = vim.opt
 
-require("which-key").setup()
-vim.keymap.set("n", "<cr>", function() require("flash").jump() end)
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+opt.scrolloff = 8
+opt.wrap = false
+opt.mouse = "a"
+opt.termguicolors = true
+opt.laststatus = 3
+opt.shortmess:append("I") -- no intro
+opt.completeopt = { "menu", "menuone", "noselect" }
 
---____OPTIONS____
-vim.opt.number = true
-vim.opt.relativenumber = true
--- vim.opt.cursorline = true
-vim.opt.signcolumn = "yes"
-vim.opt.scrolloff = 8
-vim.opt.wrap = false
-vim.o.winborder = "rounded"
-vim.opt.shortmess:append("I") -- disable starter info
+-- Indentation
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.smartindent = true
 
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.smartindent = true
+-- File handling
+opt.undofile = true
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.swapfile = false
+opt.backup = false
+opt.autoread = true
 
-vim.opt.undofile = true
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.backup = false
-vim.opt.swapfile = false
-vim.opt.autoread = true
+-- Performance
+opt.lazyredraw = true
+opt.synmaxcol = 240
+opt.updatetime = 200
+opt.ttimeoutlen = 10
+opt.mousescroll = "ver:5,hor:0"
 
-vim.opt.termguicolors = true
-vim.opt.laststatus = 3
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
+-- UI
+opt.winborder = "rounded"
 
-vim.opt.mouse = "a"
-vim.opt.clipboard = "unnamedplus"
-
-vim.opt.lazyredraw = true
-vim.opt.synmaxcol = 240
-vim.opt.updatetime = 200
-vim.opt.ttimeoutlen = 10
-vim.opt.mousescroll = "ver:5,hor:0"
-
+-------------------------------------------------
+-- Diagnostics
+-------------------------------------------------
 vim.diagnostic.config({
-  virtual_text = { current_line = true }
+  virtual_text = { current_line = true },
+  float = { border = "rounded" },
 })
 
+-------------------------------------------------
+-- Autocmds
+-------------------------------------------------
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
   end,
 })
 
---____KEYMAPS___
+-------------------------------------------------
+-- Keymaps
+-------------------------------------------------
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- NORMAL MODE
-map("n", "<esc>", "<cmd>noh<cr>", opts)
+-- General
+map("n", "<Esc>", "<cmd>nohlsearch<cr>", opts)
+map("n", "<C-s>", "<cmd>w<cr>", opts)
+map("n", "<leader>w", "<cmd>w<cr><cmd>lua vim.lsp.buf.format({ async = true })<cr>", { desc = "Save & Format" })
+map("n", "<leader>o", "<cmd>update<cr>:source<cr>", { desc = "Save & Reload Config" })
+map("n", "<C-q>", "<cmd>q<cr>", opts)
+map("n", "<leader>a", "ggVG", { desc = "Select All" })
 
-map("n", "<leader>o", "<cmd>update<cr> :source<cr>", opts)
-map("n", "<c-q>", "<cmd>quit<cr>", opts)
-map("n", "<leader>w", "<Esc><cmd>w<CR><cmd>lua vim.lsp.buf.format({ async = true })<CR>", { desc = "Save & Format" })
-map("n", "<C-s>", "<cmd>write<cr>", opts)
+-- Clipboard
+map({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to System Clipboard" })
+map("n", "<leader>Y", [["+Y]], { desc = "Yank Line to Clipboard" })
+map({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste from System Clipboard" })
+map("n", "<C-c>", "<cmd>%y+<cr>", { desc = "Copy File to Clipboard" })
 
--- Window navigation
+-- Buffers
+map("n", "<Tab>", "<cmd>bnext<cr>", opts)
+map("n", "<S-Tab>", "<cmd>bprevious<cr>", opts)
+map("n", "<leader>x", "<cmd>bdelete!<cr>", { desc = "Close Buffer" })
+
+-- Movement
 map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-j>", "<C-w>j", opts)
 map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 
--- Search + scroll
 map("n", "n", "nzzzv", opts)
 map("n", "N", "Nzzzv", opts)
 map("n", "<C-d>", "<C-d>zz", opts)
 map("n", "<C-u>", "<C-u>zz", opts)
 
 -- Move lines
-map("n", "<A-k>", ":m .-2<CR>==", opts)
-map("n", "<A-j>", ":m .+1<CR>==", opts)
+map("n", "<A-j>", ":m .+1<cr>==", opts)
+map("n", "<A-k>", ":m .-2<cr>==", opts)
+map("i", "<A-j>", "<esc>:m .+1<cr>==gi", opts)
+map("i", "<A-k>", "<esc>:m .-2<cr>==gi", opts)
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", opts)
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", opts)
 
--- Select all / copy
-map("n", "<leader>a", "ggVG", opts)
-map("n", "<c-c>", "<cmd>%y+<cr>", opts)
+-- Edit & Replace
 map("n", "x", '"_x', opts)
-map("x", "c", '"_c', opts)
+map("v", "x", '"_x', opts)
+map("v", "c", '"_c', opts)
 
--- Buffers
-map("n", "<tab>", "<cmd>bnext<cr>", opts)
-map("n", "<s-tab>", "<cmd>bprevious<cr>", opts)
-map("n", "<leader>x", "<cmd>bdelete!<cr>", opts)
+map("n", "<leader>fr", function()
+  local find = vim.fn.input("Find: ")
+  local replace = vim.fn.input("Replace: ")
+  vim.cmd(string.format("%%s/%s/%s/gc", find, replace))
+end, { desc = "Find & Replace" })
 
--- Yank & paste
-map("n", "<leader>y", 'm`"0yy"0p``', opts)
-
-map("n", "<C-j>", function()
-  vim.diagnostic.jump({ count = 1 })
-end, opts)
-
-map("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, { desc = "LSP Format" })
-map("n", "<leader>li", "g=G``", { desc = "Indent file" })
-
+-- Diagnostics
+map("n", "<C-j>", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Next Diagnostic" })
+map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show Diagnostic Float" })
 map("n", "<leader>cd", function()
   local diags = vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
   if #diags > 0 then
     vim.fn.setreg("+", table.concat(vim.tbl_map(function(d) return d.message end, diags), "\n"))
-    vim.notify("Diagnostics copied", vim.log.levels.INFO)
+    vim.notify("Diagnostics copied to clipboard", vim.log.levels.INFO)
   else
-    vim.notify("No diagnostics", vim.log.levels.INFO)
+    vim.notify("No diagnostics on this line", vim.log.levels.INFO)
   end
-end, { desc = "Copy line diagnostics" })
+end, { desc = "Copy Line Diagnostics" })
 
--- Find & replace
-map("n", "<leader>fr", function()
-  local find = vim.fn.input("Find: ")
-  local replace = vim.fn.input("Replace with: ")
-  vim.cmd(string.format("%%s/%s/%s/gc", find, replace))
-end, { desc = "Find and replace" })
+-- LSP
+map("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, { desc = "LSP Format" })
+map("n", "<leader>li", "g=G``", { desc = "Indent File" })
 
-map("n", "<leader>fc", function()
-  vim.cmd("e " .. vim.fn.stdpath("config"))
-end, { desc = "Open config" })
+-- Config
+map("n", "<leader>fc", function() vim.cmd("e " .. vim.fn.stdpath("config")) end, { desc = "Open Config" })
 
--- INSERT MODE
-map("i", "<A-k>", "<esc>:m .-2<CR>==gi", opts)
-map("i", "<A-j>", "<esc>:m .+1<CR>==gi", opts)
+-- Insert mode
 map("i", "jk", "<esc>", opts)
 map("i", "kj", "<esc>", opts)
-map("i", "<C-s>", "<Esc><cmd>w<CR><cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
-
--- VISUAL MODE
-map("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
-map("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-map("v", "<leader>y", '"1y`>"0p', opts)
-map("v", "x", '"_x', opts)
-map("v", "c", '"_c', opts)
-
-map("x", "<leader>y", '"1y`>"0p', opts)
+map("i", "<C-s>", "<Esc><cmd>w<cr><cmd>lua vim.lsp.buf.format({ async = true })<cr>", opts)
